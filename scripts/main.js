8979723c0020f5e92dfeb9c1fa14e20fe32f9a3a
Katie <katherinelane11@gmail.com>
2017-07-18 13:26:13 -0400

    finishes general JS for click of picture

--- scripts/main.js
+++ scripts/main.js
@@ -2,54 +2,56 @@
 // replace main image with clicked image
 // replace text with image
 
+
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 // get trigger link
 var THUMBNAIL_LINK_SELECTOR = "[data-image-role='trigger']";
 
-
-
-
-
-//2
-function arrayMaker(){
-    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-    var thumbnailsArray = [].slice.call(thumbnails);
-    return thumbnailsArray;
-};
-
 // take thumbnailsArray and pull out the information I need
-
-//1
-function initializeEvent(){
-    var thumbnailsArray = arrayMaker();
-    //for each time clicked, must make click function//
-    thumbnailsArray.forEach(clickTrigger);
-
+//7
+function setDetails(imageUrl, titleText){
+    var detailsImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    // method Element.setAttribute(name: string, value: string)
+    detailsImage.setAttribute("src", imageUrl)
+    var detailsText = document.querySelector(DETAIL_TITLE_SELECTOR);
+    // method Node.textContent = string
+    detailsText.textContent = titleText;
 }
 
-//3
-function clickTrigger(thumb){
-    thumb.addEventListener("click", function (event){
-        event.preventDefault();
-        setDetailsForThumb(thumb);
-    })
-}
-
-//4
-function setDetailsforThumb(thumbnail){
-    setDetails(getImage(thumbnail), getText(thumbnail));
-}
-
-//5.2
+//6.2
 function getImage(thumbnail){
     return thumbnail.getAttribute("data-image-url");
 }
 
-//5.1
+//6.1
 function getText(thumbnail){
     return thumbnail.getAttribute("data-image-title");
 }
-
-//6
-function setDetails(imageUrl, titleText){
-
+//5*
+function setDetailsForThumb(thumbnail){
+    return setDetails(getImage(thumbnail), getText(thumbnail));
 }
+//4*
+function clickTrigger(thumb){
+    thumb.addEventListener("click", function (event){
+        event.preventDefault();
+        var thing1 = setDetailsForThumb(thumb);
+    });
+}
+//3 *
+function arrayMaker(){
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailsArray = [].slice.call(thumbnails);
+    return thumbnailsArray;
+}
+
+//2*
+function initializeEvent(){
+    var thumbnailsArray = arrayMaker();
+    //for each time clicked, must make click function//
+    thumbnailsArray.forEach(clickTrigger);
+}
+
+//1*
+initializeEvent();
